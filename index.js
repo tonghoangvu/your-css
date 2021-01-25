ea1531528e4d00ed776a27a274455b95ddd16e09
tonghoangvu <tonghoangvu@outlook.com>
2021-01-25 13:59:50 +0700

    Add support for getting system theme

--- index.js
+++ index.js
@@ -1,83 +1,113 @@
+// Elements
 const rangeFontSize = document.getElementById('range-font-size');
 const textFontSize = document.getElementById('text-font-size');
-const buttonSwitchTheme = document.getElementById('button-switch-theme');
 const selectPrimaryColor = document.getElementById('select-primary-color');
+const selectTheme = document.getElementById('select-theme');
 
-let darkMode = false;
-let fontSize = 16;
-let primaryColor = 'blue';
+// Properties
+const config = {
+    fontSize: 16,
+    primaryColor: 'blue',
+    theme: 'system'
+};
 
-// AUTORUN
+// Tasks
+loadConfig(defaultConfig);
+loadConfig(baseTheme);
+loadPreviousSettings();
+reloadFontSize();
+reloadPrimaryColor();
+reloadTheme();
+setSystemThemeListener(reloadTheme);  // Track system theme change
+assignEvents();
 
-(function load() {
-    darkMode = parseInt(localStorage.getItem('dark-mode')) == 1;
-    fontSize = parseInt(localStorage.getItem('font-size')) || 16;
-    primaryColor = localStorage.getItem('primary-color') || 'blue';
-    loadConfig(defaultConfig);
-    loadConfig(baseTheme);
-    reloadFontSize();
-    reloadPrimaryColor();
-    reloadTheme();
-})();
+/**
+ * General functions
+ */
 
-(function assignEvents() {
+function loadPreviousSettings() {
+    config.fontSize = parseInt(localStorage.getItem('font-size')) || 16;
+    config.primaryColor = localStorage.getItem('primary-color') || 'blue';
+    config.theme = localStorage.getItem('theme') || 'system';
+}
+
+function assignEvents() {
     rangeFontSize.addEventListener('change', changeFontSize);
-    buttonSwitchTheme.addEventListener('click', switchTheme);
     selectPrimaryColor.addEventListener('change', changePrimaryColor);
-})();
+    selectTheme.addEventListener('change', changeTheme);
+}
 
-// INTERNAL
+/**
+ * Event handlers
+ * - Update config to match element values
+ * - Save new config to localStorage
+ * - Reload UI
+ */
+
+function changeFontSize(event) {
+    config.fontSize = event.target.value;
+    localStorage.setItem('font-size', config.fontSize);
+    reloadFontSize();
+}
+
+function changePrimaryColor(event) {
+    config.primaryColor = event.target.value;
+    localStorage.setItem('primary-color', config.primaryColor);
+    reloadPrimaryColor();
+    reloadTheme();  // Because active theme are modified, reload needed
+}
+
+function changeTheme(event) {
+    config.theme = event.target.value;
+    localStorage.setItem('theme', config.theme);
+    reloadTheme();
+}
+
+/**
+ * Update UI using config values
+ * - Reload your-css internal
+ * - Update on elements
+ */
 
 function reloadFontSize() {
-    changeCSSVariable('--base-font-size', fontSize + 'px');
-    rangeFontSize.value = fontSize;
-    textFontSize.innerText = 'Base font size: ' + fontSize + 'px';
+    changeCSSVariable('--base-font-size', config.fontSize + 'px');
+    rangeFontSize.value = config.fontSize;
+    textFontSize.innerText = 'Base font size: ' + config.fontSize + 'px';
 }
 
 function reloadPrimaryColor() {
     modifyConfig(lightTheme, {
-        '--base-color-0': 'var(--' + primaryColor + '-500)',
-        '--base-color-1': 'var(--' + primaryColor + '-600)',
-        '--base-color-2': 'var(--' + primaryColor + '-700)',
+        '--base-color-0': 'var(--' + config.primaryColor + '-500)',
+        '--base-color-1': 'var(--' + config.primaryColor + '-600)',
+        '--base-color-2': 'var(--' + config.primaryColor + '-700)',
     });
     modifyConfig(darkTheme, {
-        '--base-color-0': 'var(--' + primaryColor + '-500)',
-        '--base-color-1': 'var(--' + primaryColor + '-400)',
-        '--base-color-2': 'var(--' + primaryColor + '-300)',
+        '--base-color-0': 'var(--' + config.primaryColor + '-500)',
+        '--base-color-1': 'var(--' + config.primaryColor + '-400)',
+        '--base-color-2': 'var(--' + config.primaryColor + '-300)',
     });
-    const optionElems = document.querySelectorAll(
-        '#' + selectPrimaryColor.id + ' > option');
-    for (let i = 0; i < optionElems.length; i++)
-        if (optionElems[i].value == primaryColor) {
-            selectPrimaryColor.selectedIndex = i;
-            break;
-        }
+    selectOptionByValue(selectPrimaryColor, config.primaryColor);
 }
 
 function reloadTheme() {
-    if (!darkMode)
+    if (config.theme == 'light')
         loadConfig(lightTheme);
-    else
+    else if (config.theme == 'dark')
         loadConfig(darkTheme);
+    else
+        loadConfig(isSystemThemeDark() ? darkTheme : lightTheme);
+    selectOptionByValue(selectTheme, config.theme);
 }
 
-// EVENTS
+/**
+ * Utils functions
+ */
 
-function changeFontSize(event) {
-    fontSize = event.target.value;
-    localStorage.setItem('font-size', fontSize);
-    reloadFontSize();
-}
-
-function switchTheme() {
-    darkMode = !darkMode;
-    localStorage.setItem('dark-mode', darkMode ? 1 : 0);
-    reloadTheme();
-}
-
-function changePrimaryColor(event) {
-    primaryColor = event.target.value;
-    localStorage.setItem('primary-color', primaryColor);
-    reloadPrimaryColor();
-    reloadTheme();
+function selectOptionByValue(selectElem, optionValue) {
+    const optionElems = document.querySelectorAll('#' + selectElem.id + ' > option');
+    for (let i = 0; i < optionElems.length; i++)
+        if (optionElems[i].value == optionValue) {
+            selectElem.selectedIndex = i;
+            break;
+        }
 }
